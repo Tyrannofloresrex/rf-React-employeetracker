3ecb78a9d48741cd8508829305b946a4da104c5b
Rex Flores <Rexgflores@gmail.com>
2021-02-17 19:56:42 -0800

    updated sort buttons

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import API from "./utils/API";
 class App extends React.Component {
   state = {
     employees: [],
+    flip: 1,
+    search: "",
   };
   componentDidMount() {
     API.generateEmployees().then((res) => {
@@ -22,22 +24,28 @@ class App extends React.Component {
     this.setState({ employees: employeeList });
   };
 
+  handleInputChange = event => {
+    this.setState({ search: event.target.value });
+  };
+
+  handleFormSubmit = event => {
+    event.preventDefault();
+    this.searchEmployees(this.state.search)
+    this.setState({search:""})
+  }
+    
   sortEmployees = (event) => {
     event.preventDefault();
     this.setState({
+      flip: this.state.flip*-1,
       employees: this.state.employees.sort((a, b) => {
         if (a.name.last < b.name.last) {
-          return a.name.last < b.name.last
-            ? -1
-            : a.name.last > b.name.last
-            ? 1
-            : 0;
-        } else {
-          return a.name.last > b.name.last
-            ? 1
-            : a.name.last < b.name.last
-            ? -1
-            : 0;
+          return -1* this.state.flip
+        } 
+        else if (a.name.last > b.name.last) {
+          return 1* this.state.flip
+        }else {
+          return 0
         }
       }),
     });
@@ -45,9 +53,14 @@ class App extends React.Component {
 
   searchEmployees = (event) => {
     event.preventDefault();
+    const employeeFilter = this.state.employees.filter(employeeFilter => 
+      employeeFilter.name.first.toLowerCase()===this.state.search.toLowerCase() || employeeFilter.name.last.toLowerCase()===this.state.search.toLowerCase()  )
     this.setState({
-      employees: this.state.employees.filter()
+      employees: employeeFilter
     })
+    // if (this.state.search === ""){
+    //   this.componentDidMount()
+    // }
   }
 
   // Map over the employees array and put their info in a card
@@ -55,7 +68,13 @@ class App extends React.Component {
     return (
       <div>
         <h1>Employee List</h1>
-        <Searchbar sortEmployees={this.sortEmployees} />
+        <Searchbar 
+        sortEmployees={this.sortEmployees} 
+        flip={this.state.flip}
+        searchEmployees = {this.searchEmployees}
+        handleFormSubmit = {this.handleFormSubmit}
+        handleInputChange = {this.handleInputChange}
+        />
         <Table />
         {this.state.employees.map((employee) => (
           <Employee
--- src/components/Searchbar/index.js
+++ src/components/Searchbar/index.js
@@ -5,7 +5,7 @@ function Searchbar (props) {
     <form>
       <input class = "search-input" type="search" name="search" />
       <button  type="submit">Search</button>
-      <button type="submit" onClick={props.sortEmployees}>Sort</button> 
+      <button type="submit" onClick={props.sortEmployees}>Sort{props.flip<0?(<i className="fas fa-arrow-alt-circle-down"></i>):(<i className="far fa-arrow-alt-circle-up"></i>)}</button> 
     </form>
   </div>
   }
